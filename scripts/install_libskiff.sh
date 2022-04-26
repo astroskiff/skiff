4f4378baedc6b064e1bc5f65c04a393129da67c9
bosley <bosley117@gmail.com>
2022-04-26 13:11:54 -0400

    circleci

--- scripts/install_libskiff.sh
+++ scripts/install_libskiff.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/astroskiff/libskiff
 cd libskiff
-mkdir build
-cd build
+mkdir libskiff_build
+cd libskiff_build
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make -j9
 sudo make install
+cd ..
--- scripts/install_libutil.sh
+++ scripts/install_libutil.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/astroskiff/libutil
 cd libskiff
-mkdir build
-cd build
+mkdir libutil_build
+cd libutil_build
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make -j9
 sudo make install
+cd ..
