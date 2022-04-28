993171ecd6c3f9b7be9e256fa6056ab5f1568428
bosley <bosley117@gmail.com>
2022-04-28 08:36:55 -0400

    io_disk with tested writing

--- src/machine/system/io_disk.cpp
+++ src/machine/system/io_disk.cpp
@@ -14,7 +14,6 @@ namespace machine {
 namespace system {
 
 namespace {
-
 enum class status_e {
   UNKNOWN_ERROR = 0,
   OKAY = 1,
@@ -79,58 +78,29 @@ public:
   std::tuple<status_e, uint64_t> open(const unsigned int flags, const std::string file_path);
 
   // Close and remove a file
-  std::tuple<status_e> close(const uint64_t id);
-
-  bool nonblocking_id_exists(const uint64_t id);
-
-  std::tuple<status_e, bool> output_string_to_file(const uint64_t id, const std::string data);
-  std::tuple<status_e, bool> output_binary_to_file(const uint64_t id, const std::string data);
+  bool close(const uint64_t id);
+  bool write(const uint64_t id, const char * data, std::size_t length);
 
 private:
   uint64_t _next_id{0};
 
-  uint64_t get_an_id();
-  bool id_exists(const uint64_t id);
-
-  std::mutex _id_mutex;
-  std::mutex _map_mutex;
+  std::mutex _mutex;
   std::queue<uint64_t> _id_recycle_bin;
   std::unordered_map<uint64_t, std::unique_ptr<file_c>>  _file_map;
 };
 
-uint64_t file_manager_c::get_an_id()
-{
-  uint64_t id{0};
-  {
-    std::lock_guard<std::mutex> lock(_id_mutex);
-    if (_id_recycle_bin.empty()) {
-      id = _id_recycle_bin.front();
-      _id_recycle_bin.pop();
-    } else {
-      id = _next_id++;
-    }
-  }
-  return id;
-}
-
-bool file_manager_c::nonblocking_id_exists(const uint64_t id) 
-{
-  return _file_map.find(id) != _file_map.end();
-}
-
-bool file_manager_c::id_exists(const uint64_t id) 
-{
-  std::lock_guard<std::mutex> lock(_id_mutex);
-  return _file_map.find(id) != _file_map.end();
-}
-
 std::tuple<status_e, uint64_t> file_manager_c::open(const unsigned int flags, const std::string file_path)
 {
-  if (!std::filesystem::is_regular_file(file_path)) {
-    return { status_e::FILE_NOT_FOUND, 0 };
+  std::lock_guard<std::mutex> lock(_mutex);
+
+  uint64_t id{0};
+  if (!_id_recycle_bin.empty()) {
+    id = _id_recycle_bin.front();
+    _id_recycle_bin.pop();
+  } else {
+    id = _next_id++;
   }
 
-  uint64_t id = get_an_id();
   file_c *file = new file_c(id, file_path, flags);
 
   if (!file->open()) {
@@ -138,51 +108,35 @@ std::tuple<status_e, uint64_t> file_manager_c::open(const unsigned int flags, co
     return { status_e::CANT_OPEN_FILE, 0};
   }
 
-  // Lock the map mutex and insert the now opened file
-  {
-    std::lock_guard<std::mutex> lock(_map_mutex);
-    _file_map.insert(std::make_pair(id, std::move(std::unique_ptr<file_c>(file))));
-  }
+  _file_map.insert(std::make_pair(id, std::move(std::unique_ptr<file_c>(file))));
 
   return { status_e::OKAY, id };
 }
 
-std::tuple<status_e> file_manager_c::close(const uint64_t id)
+bool file_manager_c::close(const uint64_t id)
 {
-  std::lock_guard<std::mutex> lock(_id_mutex);
+  std::lock_guard<std::mutex> lock(_mutex);
   auto it = _file_map.find(id);
   if (it == _file_map.end()){
-    return {status_e::UNKNOWN_ID};
+    return true;
   }
   _file_map[id].get()->close();
   _file_map.erase(it);
-  return {status_e::OKAY};
+  _id_recycle_bin.push(id);
+  return true;
 }
 
-std::tuple<status_e, bool> file_manager_c::output_string_to_file(const uint64_t id, const std::string data)
+bool file_manager_c::write(const uint64_t id, const char * data, std::size_t length)
 {
-  if(!nonblocking_id_exists(id)) {
-    return {status_e::UNKNOWN_ID, false};
+  std::lock_guard<std::mutex> lock(_mutex);
+  if (!data || length == 0) {
+    return false;
   }
-
-  // Not blocking the map here on write so we don't lock all operations
-  // might want to rethink how to do this. 
-
-  _file_map[id].get()->write(data.c_str(), data.size());
-  return {status_e::OKAY, true};
-}
-
-std::tuple<status_e, bool> file_manager_c::output_binary_to_file(const uint64_t id, const std::string data)
-{
-  if(!nonblocking_id_exists(id)) {
-    return {status_e::UNKNOWN_ID, false};
+  if(_file_map.find(id) == _file_map.end()) {
+    return false;
   }
-
-  // Not blocking the map here on write so we don't lock all operations
-  // might want to rethink how to do this. 
-
-  _file_map[id].get()->write(reinterpret_cast<const char *>(&data[0]), data.size());
-  return {status_e::OKAY, true};
+  _file_map[id].get()->write(data, length);
+  return true;
 }
 
 io_disk_c::io_disk_c() : _manager(new file_manager_c())
